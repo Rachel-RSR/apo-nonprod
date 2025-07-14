517f65823d2549404018c487550b85a8bfcf21a5
Rachel-RSR <rachel_ramsaran@hotmail.com>
2025-07-13 22:19:01 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-  let currentSlide = 0;
-  const slides = document.querySelectorAll('.slide');
 
-  setInterval(() => {
-    slides[currentSlide].classList.remove('active');
-    currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].classList.add('active');
-  }, 5000);
-});
